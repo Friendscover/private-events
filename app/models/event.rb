d5c1d47cefbcb0aa3cf4099d7420325d9e3921a4
Tobias Klöpper <tobiaskloepper1@web.de>
2021-02-07 20:22:00 +0100

    Events now show attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  scope :past, -> { where("date < ?", DateTime.now.to_s(:number)) }
+  scope :upcoming, -> { where("date > ?", DateTime.now.to_s(:number)) }
+
   belongs_to :creator, class_name: "User"
   has_many :event_attendees, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendees, source: :attendee
